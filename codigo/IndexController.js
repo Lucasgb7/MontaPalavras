2cb5837c0eaad5eef74115c45d8dfd35c3dcb763
jonath <jonathherdt@gmail.com>
2019-04-01 17:57:33 -0300

    Fazendo gambiarra pro lukinha encher o saco dps

--- codigo/IndexController.js
+++ codigo/IndexController.js
@@ -1,61 +1,61 @@
 app.controller('IndexController', function($scope) {
     $scope.imagens_palavras = [{
             palavra: "AVIÃO",
-            letras: "MÍABVÃOWI",
+            letras: "ÃWAMÍVBIO",
             imagem: "../imagens/aviao.png",
             completada: false
         },
         {
             palavra: "BICICLETA",
-            letras: "IÇCENLBVTÁIA",
+            letras: "ÇBVICÁILTETNA",
             imagem: "../imagens/bicicleta.png",
             completada: false
         },
         {
             palavra: "BOLA",
-            letras: "JLTARBOÃ",
+            letras: "JBROÃLTA",
             imagem: "../imagens/bola.png",
             completada: false
         },
         {
             palavra: "CACHORRO",
-            letras: "PCQRJOCHAARÔÃ",
+            letras: "ÔCQHARÃPOJ",
             imagem: "../imagens/cachorro.png",
             completada: false
         },
         {
             palavra: "CARRO",
-            letras: "CVORAARÇU",
+            letras: "KCVARÇOU",
             imagem: "../imagens/carro.png",
             completada: false
         },
         {
             palavra: "GALINHA",
-            letras: "BRIAZLGQTNAH",
+            letras: "TAZLQNRHIBG",
             imagem: "../imagens/galinha.png",
             completada: false
         },
         {
             palavra: "LEÃO",
-            letras: "ÕÃÊEILO",
+            letras: "IÃLOÕEÊ",
             imagem: "../imagens/leao.png",
             completada: false
         },
         {
             palavra: "PÁSSARO",
-            letras: "FAÁLARSPA",
+            letras: "LÁCARFPOAS",
             imagem: "../imagens/passaro.png",
             completada: false
         },
         {
             palavra: "PEIXE",
-            letras: "VEVBXIPLE",
+            letras: "BPIQXLÊEV",
             imagem: "../imagens/peixe.png",
             completada: false
         },
         {
             palavra: "QUEIJO",
-            letras: "KYEXOQJAÍIÛU",
+            letras: "ÍQYEJKOUXAÛI",
             imagem: "../imagens/queijo.png",
             completada: false
         }
@@ -63,62 +63,84 @@ app.controller('IndexController', function($scope) {
     $scope.faseDaVez = selecionaNovaFase() // Define a fase inicial
 
     function selecionaNovaFase() { // Seleciona uma nova fase
-        if (!$scope.imagens_palavras.find(function(e) { return e.completada === false })) { zerouJogo() }
+        if (!$scope.imagens_palavras.find(function(e){ 
+            return e.completada === false})) { zerouJogo() }
         // Verifica se todas as fases já foram completas
-        do {
+        do{
             var indice = Math.floor(Math.random() * $scope.imagens_palavras.length) // Pega um indice pelo vetor de palavras
         } while ($scope.imagens_palavras[indice].completada) // Verifica se já foi completada
-
+        
         return $scope.imagens_palavras[indice] // Retorna a que não foi completada
     }
 
-    function zerouJogo() {
-        // Interface para qundo ele completou o jogo
+    function zerouJogo(){
+        window.location = 'congratulations.html'
     }
 
-    $scope.addLetra = function(letra) {
-        for (var i = 0; i < $scope.faseDaVez.palavra.length; i++) {
-            var elemento = 'preencher' + i;
-            if (document.getElementById(elemento).innerHTML == "") {
-                document.getElementById(elemento).innerHTML = letra;
-                break;
+    $scope.addLetra = function(letra){
+        for(var i = 0; i < $scope.faseDaVez.palavra.length; i++){
+            var elemento = 'preencher' + i
+            if(document.getElementById(elemento).innerHTML == ""){
+                document.getElementById(elemento).innerHTML = letra
+                break
             }
         }
     }
 
-
-    $scope.removeLetra = function(index) {
+    $scope.removeLetra = function(index){
         var elemento = 'preencher' + index
         document.getElementById(elemento).innerHTML = ""
         document.getElementById(elemento).className = "mostrarLetras"
-
     }
 
-    $scope.confirma = function() {
-        var palavraPreenchida = "";
+    $scope.confirma = function(){
+        if(document.getElementById('btn-confirma').innerHTML == 'Verificar Ortografia'){
+            $scope.corrige();
+        }else{
+            $scope.next();
+        }
+    }
 
-        for (var i = 0; i < $scope.faseDaVez.palavra.length; i++) {
-            var elemento = "preencher" + i;
+    $scope.corrige = function(){
+        var palavraPreenchida = ""
+
+        for(var i = 0; i < $scope.faseDaVez.palavra.length; i++){
+            var elemento = "preencher" + i
             palavraPreenchida += document.getElementById(elemento).innerHTML;
         }
-        if (palavraPreenchida == $scope.faseDaVez.palavra) {
-            var pontuacao = parseInt(document.getElementById('pontuacao').innerHTML, 10) + $scope.faseDaVez.palavra.length;
-            document.getElementById('pontuacao').innerHTML = pontuacao;
-
-            for (var i = 0; i < $scope.faseDaVez.palavra.length; i++) {
-                var elemento = 'preencher' + i;
-                document.getElementById(elemento).innerHTML = "";
+        if(palavraPreenchida == $scope.faseDaVez.palavra){
+            for(var i = 0; i < $scope.faseDaVez.palavra.length; i++){
+                var elemento = "preencher" + i
+                document.getElementById(elemento).className = 'mostrarLetrasCerto'
             }
-            alert("VOCCE EH MTT ESPECIAL!!!");
-            $scope.faseDaVez = selecionaNovaFase()
 
-        } else {
-            for (var i = 0; i < $scope.faseDaVez.palavra.length; i++) {
-                var elemento = 'preencher' + i;
+            document.getElementById('btn-confirma').innerHTML = 'Avançar'          
+        }else{
+            for(var i = 0; i < $scope.faseDaVez.palavra.length; i++){
+                var elemento = 'preencher' + i
                 if (document.getElementById(elemento).innerHTML != $scope.faseDaVez.palavra.charAt(i)) {
-                    document.getElementById(elemento).className = 'mostrarLetrasErrado';
+                    document.getElementById(elemento).className = 'mostrarLetrasErrado'
+                }else{
+                    document.getElementById(elemento).className = 'mostrarLetrasCerto'
                 }
             }
         }
+    }
+
+    $scope.next = function(){
+        var pontuacao = parseInt(document.getElementById('pontuacao').innerHTML, 10) + $scope.faseDaVez.palavra.length
+        document.getElementById('pontuacao').innerHTML = pontuacao
+
+        for(var i = 0; i < $scope.faseDaVez.palavra.length; i++){
+            var elemento = 'preencher' + i
+            document.getElementById(elemento).innerHTML = ""
+            document.getElementById(elemento).className = 'mostrarLetras'
+        }
+        
+        document.getElementById('btn-confirma').innerHTML = 'Verificar Ortografia'
+
+        $scope.faseDaVez.completada = true
+
+        $scope.faseDaVez = selecionaNovaFase()
     }
 })
